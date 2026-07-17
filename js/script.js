51ba77fc0578480b502938fb4304cb5e8befc81e
MyT1en <120334637+MyT1en@users.noreply.github.com>
2026-07-17 17:42:43 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function showFunFact() {
+  alert("My favorite sweet food as a kid was Black Bean Sticky Rice. I want to try it paired with Mangoes.");
+}
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+function changeHeading() {
+  var heading = document.getElementById("changing-heading");
+  heading.textContent = "Thanks for stopping by my page!";
+}
